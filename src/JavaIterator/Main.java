ecb6b7755f042064947c0b574109730ce7a1e1f3
Supaada-q214 <Supaada-q214@LAPTOP-O0E47377.mshome.net>
2019-11-21 19:44:32 -0600

    Ejercicios HackerRank

--- src/JavaIterator/Main.java
+++ src/JavaIterator/Main.java
@@ -12,12 +12,8 @@ public class Main {
     static Iterator func(ArrayList mylist) {
         Iterator it = mylist.iterator();
         while (it.hasNext()) {
-            Object element = it.hasNext();
-            
-            if( element instanceof String){
-                
-            }
-            
+            Object element = it.next();
+            if (element instanceof String)
             break;
         }
         return it;
---
+++ src/JavaList/Solution.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JavaList;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Supaada-q214
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        
+    }
+}
+
